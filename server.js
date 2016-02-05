5a49897e8fb874410c1da01afb4692fd74fb16c3
Tomas Vega <tomasvegagalvez@gmail.com>
2016-02-04 23:22:50 -0800

    first commit

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-var http = require('http'),
-    twit = require('twit');
 
+var http = require('http'),
+    twit = require('twit'),
+    client = require('twilio')("ACf3e2774672baa3f68d9b80591c6667df", "1b9d6ce872a880a96e6c4293c52606fc"),
+    express = require('express');
 
 var readline = require('readline');
 var rl = readline.createInterface({
@@ -24,8 +26,6 @@ function publish(message) {
     console.log(message);
     T.post('statuses/update', { status: message },  function(error, tweet, response) {
         if(error) throw error;
-        console.log(tweet);  // Tweet body. 
-        console.log(response);  // Raw response object. 
     });
 }
 
@@ -33,10 +33,66 @@ rl.on('line', function(line) {
     publish(line);
 });
 
-var http = require('http');
+function isPhoneNumber(element) {
+    return element.match(/\d/g).length===10;
+}
 
-var server = http.createServer(function(req, res) {
-    res.writeHead(200);
-    res.end('Sinapsis Colectiva');
+function sendMessage(from, msg) {
+    var words = msg.split(" ");
+    var to;
+    if (isPhoneNumber(words[0]) == -1) {
+        console.log("incorrecto");
+        to = from;
+        msg = "Wrong number format, please send just the 10 numbers";
+    } else {
+        to = "+1" + words[0];
+        msg = words.splice(1, words.length).join(" ");
+        publish(msg);
+    }
+    console.log("to: " + to);
+    console.log("msg: " + msg);
+    client.messages.create({ 
+        to: to, 
+        from: "+18556190666", 
+        body: msg 
+    }, function(err, message) { 
+        if (err) console.log(err);
+    });
+    if (to == from) return;
+    var response = 'Enviame la continuacion de la historia, incluyendo en el mensaje el numero de tu amigo que va a continuar la historia. No te olvides de agregar el hashtag!';
+    setTimeout(function () {
+        client.messages.create({ 
+            to: to, 
+            from: "+18556190666", 
+            body: response 
+        }, function(err, message) { 
+            if (err) console.log(err);
+        });
+    }, 5000);
+}
+
+var app = express(),
+    bodyParser = require('body-parser'),
+app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.post('/incoming', function(req, res) {
+    var data = req.body,
+        from = data.From,
+        msg = data.Body;
+    console.log("from: " + from);
+    console.log("msg: " + msg);
+    var response = '<Response><Message>Gracias por contribuir al cadaver exquisito!</Message></Response>';
+    sendMessage(from, msg);
+    res.send(response);
 });
-server.listen(8080);
+
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+
+module.exports = app;
